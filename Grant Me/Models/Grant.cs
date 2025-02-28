f1d71c6dd824873dfee34f0948fb106c6c8b238b
Dizzyio <157419812+Dizzyio@users.noreply.github.com>
2025-02-27 21:51:55 -0700

    made some things required

--- Grant Me/Models/Grant.cs
+++ Grant Me/Models/Grant.cs
@@ -3,9 +3,8 @@
     public class Grant
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string EligibilityCriteria { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string EligibilityCriteria { get; set; }
         public decimal Amount { get; set; }
     }
-}
--- Grant Me/Models/User.cs
+++ Grant Me/Models/User.cs
@@ -1,10 +1,7 @@
-﻿namespace Grant_Me.Models
+﻿public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public bool IsEligible { get; set; }
-    }
+    public int Id { get; set; }
+    public required string FullName { get; set; }
+    public required string Email { get; set; }
+    public bool IsEligible { get; set; }
 }
